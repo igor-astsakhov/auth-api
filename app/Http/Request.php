8bfaad4586ec9d40eff41617fcd6704f140f79ce
Igor Astakhov <astahov@gmail.com>
2022-08-06 00:10:20 -0400

    first commit

---
+++ app/Http/Request.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Http\Request as BaseRequest;
+use Illuminate\Support\Str;
+
+class Request extends BaseRequest
+{
+    public function wantsJson(): bool
+    {
+        return Str::startsWith( $this->path(), 'api/') || parent::wantsJson();
+    }
+}
